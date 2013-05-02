1934183545c8e9195eccc660be3d0c4ea3cf497b
Ryan <ryan@ryanjm.com>
2013-05-02 16:13:04 -0600

    Added CanCan
    And locked off ability to manage posts.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130429201755) do
+ActiveRecord::Schema.define(:version => 20130502211648) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(:version => 20130429201755) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "role"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
