d0c76146ac8d94195008a4fe73eed15a0cdfea84
Mike Jewett <mikesjewett@gmail.com>
2013-09-06 11:23:10 -0400

    Fixed failing condition render in posts#update

--- app/controllers/topics/posts_controller.rb
+++ app/controllers/topics/posts_controller.rb
@@ -42,7 +42,7 @@ class Topics::PostsController < ApplicationController
       redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
-      render :new
+      render :edit
     end
   end
 
